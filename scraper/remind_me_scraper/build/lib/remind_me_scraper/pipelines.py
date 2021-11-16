7ce625c3e4d02f405a078ae3d988cf0564013a79
denzel hooke <denzelhooke@hotmail.com>
2021-11-16 12:30:38 -0600

    Added dockerfile changes

--- scraper/remind_me_scraper/build/lib/remind_me_scraper/pipelines.py
+++ scraper/remind_me_scraper/build/lib/remind_me_scraper/pipelines.py
@@ -11,19 +11,21 @@ from itemadapter import ItemAdapter
 
 # useful for handling different item types with a single interface
 
-r = redis.Redis(host='redis', port=6379, db=0)
 
 class RemindMeScraperPipeline:
     def open_spider(self, spider):
+        self.r = redis.Redis(host='redis', port=6379, db=0)
+        self.r.set('test', 99)
         print("Spider Opened")
 
-    def process_item(self, item, spider):
-        print(f'---------------------------------------------------item: {item}')
-        item = dict(item)
-        uuid = item['uuid']
-        json_response = json.dumps(item)
-        r.set(uuid, json_response)
-        r.expire(uuid, 15)
+    def process_item(self, item):
+        r = redis.Redis(host='redis', port=6379, db=0)
+        r.set('process', 1111)
+        # print(f'--item: {item}')
+        # item = dict(item)
+        # uuid = item['uuid']
+        # json_response = json.dumps(item)
+        # r.expire(uuid, 15)
         return item
 
 
