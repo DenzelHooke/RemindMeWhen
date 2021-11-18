89b8c478099b5a4078aec5d75a827a52dd176a0f
denzelH-laptop <denzelhooke@hotmail.com>
2021-11-18 00:23:33 -0600

    Changed docker-compose volume paths to Linux struc

--- web/remind_me_django/remind_me_django/celery.py
+++ web/remind_me_django/remind_me_django/celery.py
@@ -26,6 +26,8 @@ from .task_funcs import ScraperUtilz
 r = redis.Redis(host='redis', port=6379, db=0)
 scrapyd_api_url = 'http://scrapy:8080'
 app = Celery('remind_me_django')
+time_to_check = timedelta(minutes=5)
+
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -40,7 +42,7 @@ app.autodiscover_tasks()
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(120, check_for_updates.s(), name='check DB every 10')
+    sender.add_periodic_task(time_to_check, check_for_updates.s(), name='check DB every 10')
 
 
 @app.task
@@ -51,14 +53,15 @@ def check_for_updates():
     # Can't compare offset aware(dt with timezone) with offset naive(dt without a timezone) datetime objects so 
     # we localize our current time into a offset aware dt object.
     all_products = Product.objects.all()
-    print(all_products)
+    utc_now = pytz.utc.localize(dt.utcnow())
+
     for product in all_products:
-        utc_now = pytz.utc.localize(dt.utcnow())
         author = product.author
 
         diff = utc_now - product.last_updated
-        if diff.total_seconds() / 60 >= 1:
+        if diff.total_seconds() / 60 >= time_to_check.seconds:
             print(f'db_periodic_checker: Product: {product.name[:20]} Last checked: {diff.total_seconds() / 60} minutes ago.')
+            current_time = pytz.utc.localize(dt.utcnow())
             scraper = ScraperUtilz()
             # Attatch an uuid to the scrapy product and store that in redis
             scraper.scrapyd_update_run(author, product)
@@ -71,10 +74,10 @@ def check_for_updates():
             new_prod_price = prod['price']
             current_prod_price = product.price
             
-            if new_prod_price != current_prod_price:
+            if new_prod_price != current_prod_price and prod['stock']:
 
                 product.price = new_prod_price
-                product.last_updated = utc_now
+                product.last_updated = current_time
                 product.save()
 
                 price_diff = current_prod_price - new_prod_price
@@ -86,7 +89,7 @@ def check_for_updates():
                         f"""
                         Hello, your product "{product.name}" has decreased in price by ${price_diff}!
 
-                                Old price: {current_prod_price} || Updated price: {new_prod_price}            
+                                Old price: {current_prod_price} || Updated price: {product.price}            
 
 
                         Here is your product page: {product.url}
@@ -103,7 +106,7 @@ def check_for_updates():
                     # price has increased
 
                     # convert to a postive difference
-                    positive_diff = price_diff * -price_diff
+                    positive_diff = price_diff * -1
 
                     # product.price = new_prod_price
                     
@@ -113,7 +116,7 @@ def check_for_updates():
                         Hello, your product "{product.name}" has increased in price by ${"%.2f" % positive_diff}!
 
                         |------------------------------------------------------------------|
-                        |Old price: {current_prod_price} || Updated price: {new_prod_price}|
+                        |Old price: {current_prod_price} || Updated price: {product.price}|
                         |------------------------------------------------------------------|
 
 
@@ -127,14 +130,15 @@ def check_for_updates():
                     print(f'db_periodic_checker: Product: "{product.name[:20]}" updated')
                     print(f'db_periodic_checker: Email sent to "{product.author.email}"')
 
-            if product.stock != prod['stock']:
+            elif not prod['stock']:
                 product.stock = prod['stock']
-                product.last_updated = utc_now
+                product.price = 0
+                product.last_updated = current_time
                 product.save()
             else:
                 # don't run a DB operation since prices haven't changed
-                print(f"""db_periodic_checker: Product: "{product.name[:20]}".. skipped. Price of product hasn't changed""")
-            
+                pass
+
             product.last_checked = utc_now
             product.save()
             time.sleep(5)
