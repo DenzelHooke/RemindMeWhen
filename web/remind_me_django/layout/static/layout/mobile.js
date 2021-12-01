ed57fd711b753ea0e249602f6082dad61c26beaf
denzel hooke <denzelhooke@hotmail.com>
2021-12-01 17:40:53 -0600

    Updated mobile front end

---
+++ web/remind_me_django/layout/static/layout/mobile.js
@@ -0,0 +1,31 @@
+const mobile_nav_btn = document.querySelector('.sidebar-open-btn');
+const mobile_nav = document.getElementById('mobile-sidebar');
+const page_body = document.getElementsByTagName('body')[0];
+
+
+
+mobile_nav_btn.addEventListener('click', openMenu);
+
+function openMenu(e){
+    console.log('clicked');
+    const mobile_classList = Array.from(mobile_nav.classList);
+    
+    // If the navbar is not closed, do this
+    if (!(mobile_classList.includes('mobile-sidebarClosed'))){
+        console.log("closing navbar");
+        mobile_nav.classList.remove('mobile-sidebarOpen');
+        mobile_nav.classList.add('mobile-sidebarClosed');
+        mobile_nav.style.width = '0%';
+        page_body.style.overflow = 'visible';
+    }else{
+        console.log("opening navbar");
+        mobile_nav.classList.remove('mobile-sidebarClosed');
+        mobile_nav.classList.add('mobile-sidebarOpen');
+        mobile_nav.style.width = '100%';
+        page_body.style.overflow = 'hidden';
+
+    }
+    console.log(page_body);
+}
+
+
